8a95c9ee735886082da25dae7ced7257086a2405
Anthony James Padavano <padavano.anthony@gmail.com>
2026-07-04 13:38:44 -0400

    rebase/resolve conflicts on organvm/peer-audited--behavioral-blockchain#725 (#772)
    limen task HEAL-rebase-organvm-peer-audited--behavioral-blockchain-725
    
    Co-authored-by: 4444J99 <4444J99@users.noreply.github.com>

--- src/api/src/modules/contracts/contracts.module.ts
+++ src/api/src/modules/contracts/contracts.module.ts
@@ -24,6 +24,7 @@ import {
 } from "../../../services/anomaly/anomaly.service";
 import { NotificationsModule } from "../notifications/notifications.module";
 import { PaymentsModule } from "../payments/payments.module";
+import { PayModule } from "../pay/pay.module";
 import { EmailModule } from "../email/email.module";
 import Redis from "ioredis";
 import { getRedisConnectionConfig } from "../../../config/queue.config";
@@ -44,6 +45,7 @@ const redisProvider = {
     ScheduleModule.forRoot(),
     NotificationsModule,
     EmailModule,
+    PayModule,
     forwardRef(() => PaymentsModule),
   ],
   controllers: [ContractsController],
